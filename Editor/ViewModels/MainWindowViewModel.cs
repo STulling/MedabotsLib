3d6bf537910a4c6a381c2df39e0d116d413b6342
Simon Tulling <simon.tulling99@gmail.com>
2023-03-25 00:57:00 +0100

    Add beginning of some UI

---
+++ Editor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Editor.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ Editor/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Editor.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
